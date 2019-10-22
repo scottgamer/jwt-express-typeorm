e590c318ba9d078382ffd3e39e805501074b8d69
Richard Muñoz <richard.munoz@crifa.com>
2019-10-22 16:16:04 -0500

    fixed checkJwt

--- src/middleware/checkJwt.ts
+++ src/middleware/checkJwt.ts
@@ -3,14 +3,14 @@ import * as jwt from "jsonwebtoken";
 import config from "../config/config";
 
 export const checkJwt = (req: Request, res: Response, next: NextFunction) => {
-  const token = <string>req.headers["auth"];
+  const token = req.headers.authorization;
   let jwtPayload;
 
   try {
-    jwtPayload = <any>jwt.verify(token, config.jwtSecret);
+    jwtPayload = jwt.verify(token, config.jwtSecret);
     res.locals.jwtPayload = jwtPayload;
   } catch (error) {
-    res.status(401).send();
+    res.status(401).send({message: "Unauthenticated", error});
     return;
   }
 
