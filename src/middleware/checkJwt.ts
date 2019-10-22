2e4973feebe4e5c978858b2b2b346d22158c4539
Richard Muñoz <richard.munoz@crifa.com>
2019-10-22 11:39:56 -0500

    added checkJwt middleware

---
+++ src/middleware/checkJwt.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from "express";
+import * as jwt from "jsonwebtoken";
+import config from "../config/config";
+
+export const checkJwt = (req: Request, res: Response, next: NextFunction) => {
+  const token = <string>req.headers["auth"];
+  let jwtPayload;
+
+  try {
+    jwtPayload = <any>jwt.verify(token, config.jwtSecret);
+    res.locals.jwtPayload = jwtPayload;
+  } catch (error) {
+    res.status(401).send();
+    return;
+  }
+
+  // token is valid for 1 hour
+  // send a new token on every request
+  const { userId, username } = jwtPayload;
+  const newToken = jwt.sign({ userId, username }, config.jwtSecret, {
+    expiresIn: "1h"
+  });
+  res.setHeader("token", newToken);
+
+  next();
+};
