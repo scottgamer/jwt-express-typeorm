b4c8ee031e34e3e522484d85d933351401c1f08d
Richard Muñoz <richard.munoz@crifa.com>
2019-10-22 12:51:13 -0500

    removed unnecesary comments

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,16 @@ import * as helmet from "helmet";
 import * as cors from "cors";
 import routes from "./routes/";
 
-//Connects to the Database -> then starts the express
 createConnection()
   .then(async connection => {
-    // Create a new express application instance
     const app = express();
 
-    // Call midlewares
     app.use(cors());
     app.use(helmet());
     app.use(bodyParser.json());
 
-    //Set all routes from routes folder
     app.use("/", routes);
-
+    
     app.listen(3000, () => {
       console.log("Server started on port 3000!");
     });
